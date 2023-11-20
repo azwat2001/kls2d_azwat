f64efa1abc16f8f0fac0caf2451f6d24719cd59c
Ardhyansyah Mualo <89835510+ArdhyMualo@users.noreply.github.com>
2023-11-20 16:38:04 +0900

    tambah tag h1

---
+++ tes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git dengan azwat</title>
+</head>
+<body>
+    
+<h1>ini adalah codingan dari pak ardhy</h1>
+
+
+</body>
+</html>
